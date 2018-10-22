3a07e9b350e2b05d2079445e4019b3b848ddb37a
sisitang <1185986972@qq.com>
2018-10-22 15:45:57 +0800

    lunbo finished

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
    <div>
      <home-header></home-header>
+     <home-swipper></home-swipper>
    </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwipper from './components/swipper'
 export default{
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwipper
   }
 }
 </script>
---
+++ src/pages/home/components/swipper.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swipper-img" :src="item.imgUrl">
+            </swiper-slide>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeSwipper',
+        data() {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop:true
+                },
+                swiperList: [{
+                        id:"0001",
+                        imgUrl:"http://img1.qunarzz.com/piao/fusion/1809/a9/9cd909d4efc12a02.jpg_890x330_cdf2b8aa.jpg"
+                    }, {
+                        id:"0002",
+                        imgUrl:"http://img1.qunarzz.com/piao/fusion/1809/f4/03da905b8b7d2202.jpg_890x330_afab380f.jpg"
+                    }, {
+                        id:"0003",
+                        imgUrl:"http://img1.qunarzz.com/piao/fusion/1806/cf/eb8c2dfa395d7e02.jpg_890x330_e5cedf73.jpg"
+                    }
+                ]
+            }
+        }
+    }
+</script>
+<style scoped>
+    .wrapper>>>.swiper-pagination-bullet-active {
+        background: #fff !important;
+    }
+
+    .wrapper {
+        width: 100%;
+        height: 31.25vw;
+    }
+
+    .wrapper .swipper-img {
+        width: 100%;
+    }
+</style>
